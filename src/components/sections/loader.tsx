bd9a0965e2cb4daa06cd92ab93315ff3481a23ee
mohamedshawky982 <mohamedshawkym90@gmail.com>
2024-05-16 14:25:28 +0300

    add loading

--- src/components/sections/loader.tsx
+++ src/components/sections/loader.tsx
@@ -1,8 +1,17 @@
-import {ActivityIndicator} from 'react-native';
+import {ActivityIndicator, I18nManager, Text, View} from 'react-native';
 import {COLORS} from '../../common';
 
 const Loader = () => {
-  return <ActivityIndicator color={COLORS.lightBackground} size={25} />;
+  return (
+    <View style={{alignItems: 'center', justifyContent: 'center'}}>
+      <ActivityIndicator color={COLORS.lightBackground} size={25} />
+      <Text style={{marginTop: 5}}>
+        {I18nManager.isRTL
+          ? 'جاري تحميل ملقات السورة'
+          : 'Downloading the chapter files'}
+      </Text>
+    </View>
+  );
 };
 
 export default Loader;
